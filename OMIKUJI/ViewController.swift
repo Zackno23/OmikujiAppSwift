9d2444a6cc2a3a218a657f9dac0605588f5c01d9
Chikara Yoshida <chikara.f.yoshida@gmail.com>
2019-06-08 13:43:55 +0900

    Initial Commit

---
+++ OMIKUJI/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  OMIKUJI
+//
+//  Created by 吉田力 on 2019/06/08.
+//  Copyright © 2019 吉田力. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
